d8c85ad8287fba45e343180679bffc7af25b6dad
Maciej Chałapuk <maciej@chalapuk.pl>
2015-09-04 04:22:59 +0200

    + gulp produces non=minified version of css

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,34 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 var config = require('./build.config');
 
 gulp.task('sass', function () {
   gulp.src(config.css.src)
-    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest(config.dir.build));
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest(config.dir.build))
+    .pipe(cssmin())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(config.dir.build))
+  ;
 });
 
 gulp.task('javascript', function() {
-  gulp.src(config.js.src)
+  var entities = gulp.src(config.js.src)
     .pipe(jshint(config.jshint))
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(uglify())
-    .pipe(gulp.dest(config.dir.build));
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(config.dir.build))
+  ;
 });
 
 gulp.task('watch', function() {
-  gulp.watch(config.css.src, ['sass'])
-  gulp.watch([ config.js.src, '.jshintrc', 'build.config.js' ], ['javascript'])
+  gulp.watch(config.css.src, ['sass']);
+  gulp.watch([ config.js.src, '.jshintrc', 'build.config.js' ], ['javascript']);
 });
 
 gulp.task('build', ['sass', 'javascript']);
